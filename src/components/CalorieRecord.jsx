72634cc655333554fb99944c494c6a2141fe29d0
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-15 15:46:01 +0200

    m: Adding JS Code To Component

--- src/components/CalorieRecord.jsx
+++ src/components/CalorieRecord.jsx
@@ -1,9 +1,16 @@
 import "./CalorieRecord.css";
 
 function CalorieRecord(props) {
+  const month = props.date.toLocaleString("default", { month: "long" });
+  const day = props.date.getDate();
+  const year = props.date.getFullYear();
   return (
     <ul className="record">
-      <li className="record-date">{props.date.toISOString()}</li>
+      <li className="record-date">
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </li>
       <li>{props.meal}</li>
       <li>{props.content}</li>
       <li className="record-calories">{props.calories}</li>
@@ -11,9 +18,9 @@ function CalorieRecord(props) {
   );
 }
 // function CalorieRecord(props) {
-//   const month = props.date.toLocaleString("default", { month: "long" });
-//   const day = props.date.getDate();
-//   const year = props.date.getFullYear();
+  // const month = props.date.toLocaleString("default", { month: "long" });
+  // const day = props.date.getDate();
+  // const year = props.date.getFullYear();
 
 //   return (
 //     <ul className="record">
