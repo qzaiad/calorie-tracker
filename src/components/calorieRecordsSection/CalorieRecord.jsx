ceab87f5c9176df11dfe5ad154b69565e69b55bc
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-18 19:56:59 +0200

    m: Conditional behavior (version 2)
    - use jsx elements: variable contains an expression
      that returns one parent element
    - if/for... are only allowed outside {}

--- src/components/calorieRecordsSection/CalorieRecord.jsx
+++ src/components/calorieRecordsSection/CalorieRecord.jsx
@@ -3,26 +3,31 @@ import CalorieRecordDate from "./CalorieRecordDate";
 import StyledRecordCell from "../common/StyledRecordCell";
 
 function CalorieRecord(props) {
+  // This is a jsx element -> equivalent to a function that returns one parent element
+  let recordContent = (
+    // <></> React fragment => bundle children under the same umbrella => () should return one parent
+    <>
+      <li>{props.meal}</li>
+      <li>{props.content}</li>
+    </>
+  );
+
+if(props.calories < 0){
+  recordContent = (
+    // keep CSS styling
+    <>
+      <li></li>
+      <li>Invalid calories</li>
+    </>
+  )
+}
+
   return (
     <ul className="record">
       <li>
         <CalorieRecordDate date={props.date}/>
       </li>
-      {
-        props.calories < 0 ? (
-          // keep CSS styling
-        <>
-          <li></li>
-          <li>Invalid calories</li>
-        </>
-        ) : (
-          // <></> React fragment => bundle children under the same umbrella => () should return one parent
-          <>
-            <li>{props.meal}</li>
-            <li>{props.content}</li>
-          </>
-        )
-      }
+      {recordContent}
       <li className="record-calories">
         <StyledRecordCell>{props.calories}</StyledRecordCell>
       </li>
