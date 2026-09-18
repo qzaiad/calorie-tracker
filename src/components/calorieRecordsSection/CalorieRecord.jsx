91e9d7052257399b8a322632a97222d636cb1e60
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-18 20:06:45 +0200

    m: Conditional behavior (version 3)
    - returning null means nothin is rendered
    - in this case the parent element renders only empty  <li></li>
      which is actually not needed

--- src/components/calorieRecordsSection/CalorieRecord.jsx
+++ src/components/calorieRecordsSection/CalorieRecord.jsx
@@ -3,31 +3,17 @@ import CalorieRecordDate from "./CalorieRecordDate";
 import StyledRecordCell from "../common/StyledRecordCell";
 
 function CalorieRecord(props) {
-  // This is a jsx element -> equivalent to a function that returns one parent element
-  let recordContent = (
-    // <></> React fragment => bundle children under the same umbrella => () should return one parent
-    <>
-      <li>{props.meal}</li>
-      <li>{props.content}</li>
-    </>
-  );
-
-if(props.calories < 0){
-  recordContent = (
-    // keep CSS styling
-    <>
-      <li></li>
-      <li>Invalid calories</li>
-    </>
-  )
-}
+  if(props.calories < 0){
+    return null;  // nothing is rendered
+  }
 
   return (
     <ul className="record">
       <li>
         <CalorieRecordDate date={props.date}/>
       </li>
-      {recordContent}
+      <li>{props.meal}</li>
+      <li>{props.content}</li>
       <li className="record-calories">
         <StyledRecordCell>{props.calories}</StyledRecordCell>
       </li>
