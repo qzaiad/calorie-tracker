b2dd216bd2f6efc5e886b9e16b2281572a19f77f
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-18 20:46:07 +0200

    m: Dynamic style change (version 2)

--- src/components/edit/CaloriesRecordEdit.jsx
+++ src/components/edit/CaloriesRecordEdit.jsx
@@ -98,6 +98,15 @@ function CaloriesRecordEdit(props) {
     }); // => re-render
   }
 
+  let calorieInputStype = {};
+  if(mealRecord.calories < 0){
+    calorieInputStype = {
+      border: "1px solid red",
+      backgroundColor: "white",
+      color: "red",
+    };
+  }
+
   return (
     <form onSubmit={onSubmitHandler}>
       <label htmlFor="date">Date:</label>
@@ -117,15 +126,7 @@ function CaloriesRecordEdit(props) {
         id="calories"
         value={mealRecord.calories}
         onChange={onCaloriesChangeHandler}
-        style={
-          mealRecord.calories < 0
-            ? {
-                border: "1px solid red",
-                backgroundColor: "white",
-                color: "red",
-              }
-            : {}
-        }
+        style={calorieInputStype}
        />
       <div className="footer">
         <button>Add Record</button>
