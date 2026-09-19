da517b84075367e36ebd207d1d29537064395076
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-19 20:44:33 +0200

    m: Using CSS modules

--- src/components/calorieRecordsSection/RecordList.jsx
+++ src/components/calorieRecordsSection/RecordList.jsx
@@ -1,30 +1,20 @@
 import CalorieRecord from './CalorieRecord'
-import styled from "styled-components";
+import styles from "./RecordList.module.css"
 
-const List = styled.ul`
-  list-style: none;
-  padding: 0;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-`;
-
-const ListItem = styled.li`
-  margin: 10px;
-`
 
 function RecordList(props) {
   return (
-    <List>
+    <ul className={styles.record_list}>
       {
         props.records.map((record) =>
           record.calories >= 0 && (
-            <ListItem key={record.id}>
+            <li key={record.id} className={styles.record_item}>
               <CalorieRecord date={record.date} meal={record.meal} content={record.content} calories={record.calories}/>
-            </ListItem>
+            </li>
           )
         )
       }
-    </List>
+    </ul>
   );
 }
 
