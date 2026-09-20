5c97a4a8c6ce7fd8fcd43a387f3f48bf8af7897e
Qassem Abu Ahmad <Qassem.Abuahmad@rohde-schwarz.com>
2026-09-20 23:47:02 +0200

    m: Add form modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,69 @@
 import { useState } from 'react';
 import ListingSection from './components/calorieRecordsSection/ListingSection';
 import CaloriesRecordEdit from './components/edit/CaloriesRecordEdit';
+import Modal from "react-modal";
+import styles from "./App.module.css";
+
+const INITIAL_RECORDS = [
+  {
+    id: 1,
+    date: new Date(2023, 2, 1),
+    meal: "Breakfast",
+    content: "Eggs",
+    calories: -340,
+  },
+  {
+    id: 2,
+    date: new Date(2023, 2, 2),
+    meal: "Lunch",
+    content: "Chicken",
+    calories: 600,
+  },
+  {
+    id: 3,
+    date: new Date(2023, 2, 3),
+    meal: "Dinner",
+    content: "Cheese",
+    calories: 200,
+  },
+  {
+    id: 4,
+    date: new Date(2023, 2, 4),
+    meal: "Snacks",
+    content: "Chocolate",
+    calories: 500,
+  },
+]
 
 function App() {
-  const INITIAL_RECORDS = [
-    {
-      id: 1,
-      date: new Date(2023, 2, 1),
-      meal: "Breakfast",
-      content: "Eggs",
-      calories: -340,
-    },
-    {
-      id: 2,
-      date: new Date(2023, 2, 2),
-      meal: "Lunch",
-      content: "Chicken",
-      calories: 600,
-    },
-    {
-      id: 3,
-      date: new Date(2023, 2, 3),
-      meal: "Dinner",
-      content: "Cheese",
-      calories: 200,
-    },
-    {
-      id: 4,
-      date: new Date(2023, 2, 4),
-      meal: "Snacks",
-      content: "Chocolate",
-      calories: 500,
-    },
-  ]
-  
   const [records, setRecords] = useState(INITIAL_RECORDS);
-  const [nextId, setNextId] = useState(INITIAL_RECORDS.length + 1)
+  const [nextId, setNextId] = useState(INITIAL_RECORDS.length + 1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      border: 'none',
+      borderRadius: 'var(--them-border-radius-smooth)',
+      padding: '0',
+    },
+    overlay: {
+      backgroundColor: 'rgbs(190, 190, 190, 0.5)',
+    }
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  }
 
   const onFormSubmitHandler = (record) => {
     // console.log(record);
@@ -48,13 +76,17 @@ function App() {
     // console.log(formattedRecord);
     setNextId(lastUsedId => lastUsedId + 1)
     setRecords(prevRecords => [formattedRecord, ...prevRecords]);
+    handleCloseModal();
   };
 
   return (
       <div className="App">
-        <h1>Welcome to React with Almdrasa!</h1>
-        <CaloriesRecordEdit onFormSubmit={onFormSubmitHandler} />
+        <h1 className={styles.title}>Calorie Tracker</h1>
+        <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal} contentLabel='Modal' style={modalStyles}>
+          <CaloriesRecordEdit onFormSubmit={onFormSubmitHandler} />
+        </Modal>
         <ListingSection allRecords={records}/>
+        <button onClick={handleOpenModal} className={styles["open-modal-btn"]}>Track food</button>
       </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Modal from "react-modal";
+
+Modal.setAppElement("#root"); // MUST BE CALLED ONLY ONCE
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
